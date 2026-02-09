005e09c0c62e282e953565876d6f56f9eab65502
ReneP92 <rene.kesenheimer@icloud.com>
2026-02-09 11:22:48 +0000

    add keybinding for current buffer search

--- lua/rene/plugins/telescope.lua
+++ lua/rene/plugins/telescope.lua
@@ -45,6 +45,7 @@ return {
       require("telescope.builtin").find_files({ cwd = vim.fn.getcwd() })
     end, { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", { desc = "Fuzzy find in current buffer" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
